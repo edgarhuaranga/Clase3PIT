a655475aad4dd8ce17d3e898f349dead64b56271
Edgar Huaranga <edhu1227@gmail.com>
2018-03-08 19:41:37 -0500

    interfaz comprador y enlazado a java

--- app/src/main/java/com/ehuaranga/clase3/MainActivity.java
+++ app/src/main/java/com/ehuaranga/clase3/MainActivity.java
@@ -2,12 +2,30 @@ package com.ehuaranga.clase3;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText editTextNombreComprador;
+    EditText editTextDNIComprador;
+    EditText editTextNacionalidadComprador;
+
+    EditText editTextNombreProducto;
+    EditText editTextBarcodeProducto;
+    EditText editTextPrecioProducto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editTextNombreProducto = findViewById(R.id.edittext_nombre_producto);
+        editTextBarcodeProducto = findViewById(R.id.edittext_barcode);
+        editTextPrecioProducto = findViewById(R.id.edittext_precio_producto);
+
+        editTextNombreComprador = findViewById(R.id.edittext_nombre_comprador);
+        editTextDNIComprador = findViewById(R.id.edittext_dni_comprador);
+        editTextNacionalidadComprador = findViewById(R.id.edittext_nacionalidad_comprador);
+
     }
 }
